11dde97dd18b337fa6b84300b85418395718baae
Luiz_Fabiano <luizf.fabiano@gmail.com>
2020-05-19 22:05:04 -0300

    Treinamento Inicial DotNetCore

---
+++ TreinaWeb.DotNetCore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Treinaweb.DotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome: ");
+            String nome = Console.ReadLine();
+            Console.Write("Digite sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Olá, {nome}");
+            Console.WriteLine($"Você tem, {idade} anos");
+        }
+    }
+}
